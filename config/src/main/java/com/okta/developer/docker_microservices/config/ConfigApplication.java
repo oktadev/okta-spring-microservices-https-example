b2ba39fbf9a85f26ed02bc548509c437cc664551
Matt Raible <matt.raible@okta.com>
2019-03-04 17:03:28 -0700

    Fix tests and HTTPS in Docker

--- config/src/main/java/com/okta/developer/docker_microservices/config/ConfigApplication.java
+++ config/src/main/java/com/okta/developer/docker_microservices/config/ConfigApplication.java
@@ -3,15 +3,12 @@ package com.okta.developer.docker_microservices.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
 }
--- school-service/src/test/java/com/okta/developer/docker_microservices/service/ServiceApplicationTests.java
+++ school-service/src/test/java/com/okta/developer/docker_microservices/service/ServiceApplicationTests.java
@@ -3,10 +3,12 @@ package com.okta.developer.docker_microservices.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations="classpath:test.properties")
 public class ServiceApplicationTests {
 
 	@Test
