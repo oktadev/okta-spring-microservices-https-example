c6b659005c8e2517e4af5eaf7b317b0287fa349f
Matt Raible <matt.raible@okta.com>
2019-03-07 09:59:09 -0500

    Updates after QA

--- school-service/src/test/java/com/okta/developer/docker_microservices/service/ServiceApplicationTests.java
+++ school-service/src/test/java/com/okta/developer/docker_microservices/service/ServiceApplicationTests.java
@@ -3,8 +3,8 @@ package com.okta.developer.docker_microservices.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.TestPropertySource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
