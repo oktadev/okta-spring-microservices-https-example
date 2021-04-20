0577296f8bae5cc2ae50f43b2855d91e69fbe7ea
Matt Raible <matt.raible@okta.com>
2021-04-20 17:46:22 -0600

    Update RestTemplate code to be thread-safe

--- school-ui/src/main/java/com/okta/developer/docker_microservices/ui/controller/SchoolController.java
+++ school-ui/src/main/java/com/okta/developer/docker_microservices/ui/controller/SchoolController.java
@@ -2,12 +2,10 @@ package com.okta.developer.docker_microservices.ui.controller;
 
 import com.okta.developer.docker_microservices.ui.dto.TeachingClassDto;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
@@ -20,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -30,7 +27,8 @@ public class SchoolController {
     private final OAuth2AuthorizedClientService authorizedClientService;
     private final RestTemplate restTemplate;
 
-    public SchoolController(OAuth2AuthorizedClientService clientService, RestTemplate restTemplate) {
+    public SchoolController(OAuth2AuthorizedClientService clientService,
+                            RestTemplate restTemplate) { // <1>
         this.authorizedClientService = clientService;
         this.restTemplate = restTemplate;
     }
@@ -42,26 +40,21 @@ public class SchoolController {
 
     @GetMapping("/classes")
     @PreAuthorize("hasAuthority('SCOPE_profile')")
-    public ResponseEntity<List<TeachingClassDto>> listClasses(
-            @AuthenticationPrincipal OAuth2AuthenticationToken authentication) {
+    public ResponseEntity<List<TeachingClassDto>> listClasses(@AuthenticationPrincipal OAuth2AuthenticationToken authentication) { // <2>
 
         OAuth2AuthorizedClient authorizedClient =
-                this.authorizedClientService.loadAuthorizedClient(
-                        authentication.getAuthorizedClientRegistrationId(),
-                        authentication.getName());
+            this.authorizedClientService.loadAuthorizedClient(
+                authentication.getAuthorizedClientRegistrationId(),
+                authentication.getName()); // <3>
 
-        OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
-        restTemplate.getInterceptors().add(getBearerTokenInterceptor(accessToken.getTokenValue()));
+        OAuth2AccessToken accessToken = authorizedClient.getAccessToken();// <4>
+        HttpHeaders headers = new HttpHeaders() {{
+            set("Authorization", "Bearer " + accessToken.getTokenValue()); // <5>
+        }};
 
         return restTemplate
-                .exchange("https://school-service/class", HttpMethod.GET, null,
-                        new ParameterizedTypeReference<List<TeachingClassDto>>() {});
-    }
-
-    private ClientHttpRequestInterceptor getBearerTokenInterceptor(String accessToken) {
-        return (request, bytes, execution) -> {
-            request.getHeaders().add("Authorization", "Bearer " + accessToken);
-            return execution.execute(request, bytes);
-        };
+            .exchange("https://school-service/class", HttpMethod.GET, new HttpEntity<String>(headers),
+                new ParameterizedTypeReference<List<TeachingClassDto>>() {
+                });
     }
 }
